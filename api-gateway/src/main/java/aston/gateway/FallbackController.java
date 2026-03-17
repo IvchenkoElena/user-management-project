b455d85b44844a7eeb9bbef14345216f1c5af48c
IvchenkoEV <pesok72@mail.ru>
2026-03-17 08:09:48 +0500

    fix

--- api-gateway/src/main/java/aston/gateway/FallbackController.java
+++ api-gateway/src/main/java/aston/gateway/FallbackController.java
@@ -1,18 +1,18 @@
 package aston.gateway;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class FallbackController {
 
-    @GetMapping(value = "/fallback/user-service", produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/fallback/user-service", produces = MediaType.APPLICATION_JSON_VALUE)
     public String userServiceFallback() {
         return "{\"message\":\"User service is temporarily unavailable\"}";
     }
 
-    @GetMapping(value = "/fallback/notification-service", produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/fallback/notification-service", produces = MediaType.APPLICATION_JSON_VALUE)
     public String notificationServiceFallback() {
         return "{\"message\":\"Notification service is temporarily unavailable\"}";
     }
